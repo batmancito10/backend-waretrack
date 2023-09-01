af443d7b449469c02896669f3f86c659e14cb926
Miguel Moreno <miguel.morenou@gnilat.com>
2023-08-31 20:01:53 -0500

    Feat - cors

--- waretrack/settings/base.py
+++ waretrack/settings/base.py
@@ -183,3 +183,23 @@ CORS_ORIGIN_WHITELIST = (
     #local
     'http://localhost',
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+]
+
+
+CORS_ALLOW_HEADERS = [
+    'Accept',
+    'Accept-Language',
+    'Content-Language',
+    'Content-Type',
+    'Authorization',
+]
